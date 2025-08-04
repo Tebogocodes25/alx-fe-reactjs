e7ea8c7ccdea4b8e20d3e0dcb6de6ab97a8ef294
Tebogocodes25 <sitholet227@gmail.com>
2025-08-04 14:42:58 +0200

    Revert "Refactor component styles and formatting for improved readability (#1)"
    This reverts commit 8bdbb7f04f1d87aa2dd1603f82ebf1052e80a6a9.

--- alx-react-app/src/components/MainContent.jsx
+++ alx-react-app/src/components/MainContent.jsx
@@ -1,9 +1,8 @@
 function MainContent() {
   return (
-    <main style={{ backgroundColor: '#f0f0f0', padding: '15px', borderRadius: '5px' }}>
-      <p>I love to visit New York, Paris, and Tokyo!</p>
-    </main>
+   <main style={{ backgroundColor: '#f0f0f0', padding: '15px', borderRadius: '5px' }}> 
+  <p>I love to visit New York, Paris, and Tokyo!</p>
+  </main>
   );
 }
-
 export default MainContent;
